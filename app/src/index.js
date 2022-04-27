cfd056b05e2ba5914cd8b1f8ef811664767f2b30
olayeni45 <olayeni45@gmail.com>
2022-04-27 10:26:51 +0100

    fix: antd import issue

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'antd/dist/antd.min.css';
 import "./index.css";
 import App from "./App";
 import { store } from "./redux/store/store";
